0b35cd94cdac29caf16e6d37e65a64d980a640a4
PsyKusss <mael.cuillier@univ-tlse3.fr>
2024-10-25 08:24:23 +0200

    Création de la classe Humain

---
+++ src/personnages/Humain.java
@@ -0,0 +1,51 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boissonFavorite;
+	private int argent;
+	
+	public Humain(String nom, String boissonFavorite, int argent) {
+        this.nom = nom;
+        this.boissonFavorite = boissonFavorite;
+        this.argent = argent;
+    }
+	
+	public String getNom() {
+		return nom;
+	}
+	public int getArgent() {
+		return argent;
+	}
+	
+	public void parler(String texte) {
+		System.out.println("(" + getNom() + ") - " + texte);
+	}
+	
+	public void direBonjour() {
+		parler("Bonjour ! Je m'appelle " + nom + " et j'aime boire du " + boissonFavorite + ".");
+	}
+	
+	public void boire() {
+		parler("Mmmm, un bon verre de " + boissonFavorite + " ! GLOUPS !");
+	}
+	
+	public void gagnerArgent(int gain) {
+		argent += gain;
+		parler("J'ai gagner " + gain + " euros !");
+	}
+	
+	public void perdreArgent(int perte) {
+		argent -= perte;
+		parler("Je n'ai plus que " + argent + " sous en poche.");
+	}
+	
+	public void acheter(String bien, int prix) {
+		if (argent >= prix) {
+			parler("J'ai " + argent +" sous en poche. Je vais pouvoir m'offrir " + bien + " à " + prix + " sous.");
+            argent -= prix;
+        } else {
+            parler("Je n'ai plus que " + argent +" sous en poche. Je ne peux même pas m'offrir " + bien + " à " + prix + " sous.");
+        }
+	}
+}
